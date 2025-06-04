6fbaf5f7cce12e183cdc115e55eedb187df4cc97
bsc-inf-09-20 <bsc-inf-09-20@unima.ac.mw>
2025-06-04 15:53:39 +0200

    changes

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/temprecCoder/TemperatureSensorManager.kt
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/temprecCoder/TemperatureSensorManager.kt
@@ -51,13 +51,15 @@ class TemperatureSensorManager(
             arrayOf(
                 Manifest.permission.BLUETOOTH_SCAN,
                 Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             )
         } else {
             arrayOf(
                 Manifest.permission.BLUETOOTH,
                 Manifest.permission.BLUETOOTH_ADMIN,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             )
         }
 
